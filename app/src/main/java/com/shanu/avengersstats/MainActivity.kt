521a5482aa7a12c2c7d463113bd0af197ec3a94f
galanteria01 <shanuu12e@gmail.com>
2020-09-27 19:00:30 +0530

    Basic working model

--- app/src/main/java/com/shanu/avengersstats/MainActivity.kt
+++ app/src/main/java/com/shanu/avengersstats/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.shanu.avengersstats
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.avengers_ticket.*
 import kotlinx.android.synthetic.main.avengers_ticket.view.*
 
@@ -25,13 +28,16 @@ class MainActivity : AppCompatActivity() {
         listOfAvengers.add(Avengers("Black Widow","Shield Spy",R.drawable.widow))
 
 
-        adapter = AvengersAdapter(listOfAvengers)
+        adapter = AvengersAdapter(this,listOfAvengers)
+        tvAvengers.adapter = adapter
     }
     class AvengersAdapter:BaseAdapter{
         var listOfAvengers = ArrayList<Avengers>()
+        var context:Context?=null
 
-        constructor(listOfAvengers:ArrayList<Avengers>):super(){
+        constructor(context:Context,listOfAvengers:ArrayList<Avengers>):super(){
             this.listOfAvengers = listOfAvengers
+            this.context = context
 
         }
 
@@ -49,10 +55,12 @@ class MainActivity : AppCompatActivity() {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             val avenger = listOfAvengers[position]
-            var myView = layoutInflater.inflate(R.layout.avengers_ticket,null)
-            myView.tvName.text = avenger.name
-            myView.tvDes.text = avenger.des
-            myView.ivBar.setImageResource(avenger.image)
+            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var myView = inflater.inflate(R.layout.avengers_ticket,null)
+            myView.tvName.text = avenger.name!!
+            myView.tvDes.text = avenger.des!!
+            myView.ivBar.setImageResource(avenger.image!!)
+            return myView
 
         }
     }
