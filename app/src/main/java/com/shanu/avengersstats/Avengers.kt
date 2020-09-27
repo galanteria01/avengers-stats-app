7e42d25131189c577bcc4158354232a46657fb81
galanteria01 <shanuu12e@gmail.com>
2020-09-27 13:33:33 +0530

    Initial commit and initialised

---
+++ app/src/main/java/com/shanu/avengersstats/Avengers.kt
@@ -0,0 +1,14 @@
+package com.shanu.avengersstats
+
+class Avengers{
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+
+    constructor(name:String,des:String,image:Int){
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+
+}
