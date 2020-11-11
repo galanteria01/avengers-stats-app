7b72111a21dfd6d4c22c7295721d59a327b163d3
shanuu <shanuu12e@gmail.com>
2020-11-11 13:09:53 +0530

    Added superhero API support

---
+++ app/src/main/java/com/shanu/avengersstats/Hero.kt
@@ -0,0 +1,32 @@
+package com.shanu.avengersstats
+
+import android.media.Image
+
+class Hero(var response:String, var id:Int, var name:String, var powerstats:List<Power>,
+            var biography:List<Bio>, var appearance:List<Appear>,var work:List<Work>
+           ,var connection:List<ConnectionHero>,var image:Image ) {
+
+}
+
+class Power(var intelligence:Int,var strength:Int,var speed:Int,var durability:Int,var power:Int,var combat:Int){
+
+}
+
+class Bio(var fullName:String,var alterEgo:String,var aliases:List<String>
+          ,var placeOfBirth:String,var firstAppearance:String
+          ,var publisher:String,var alignment:String){
+
+}
+
+class Appear(var gender:String,var race:String,var height:List<String>
+             ,var weight:List<String>,var eyeColor:String,var hairColor:String){
+
+}
+
+class Work(var occupation:String,var base:String){
+
+}
+
+class ConnectionHero(var groupAffiliation:String,var relatives:String){
+
+}
--- app/src/main/java/com/shanu/avengersstats/MainActivity.kt
+++ app/src/main/java/com/shanu/avengersstats/MainActivity.kt
@@ -14,6 +14,8 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ProgressBar
 import androidx.core.content.ContextCompat.startActivity
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.avengers_ticket.*
 import kotlinx.android.synthetic.main.avengers_ticket.view.*
@@ -21,40 +23,49 @@ import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
+import java.net.URI.create
 import java.util.EnumSet.range
 
-class MainActivity() : AppCompatActivity(), Parcelable {
-    lateinit var progress: ProgressBar
+class MainActivity() : AppCompatActivity() {
     var listOfAvengers = ArrayList<Avengers>()
-    var adapter:AvengersAdapter?=null
-    val client = OkHttpClient()
+    var adapter: AvengersAdapter? = null
 
-    constructor(parcel: Parcel) : this() {
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         dummyData()
-        progress = findViewById(R.id.progressBar)
-        progress.visibility = View.VISIBLE
-
-        // start loading avengers
-        run("https://superheroapi.com/api/access-token/character-id")
-
-
-        adapter = AvengersAdapter(this,listOfAvengers)
+        adapter = AvengersAdapter(this, listOfAvengers)
         tvAvengers.adapter = adapter
+
+        fetchJson()
+    }
+
+    fun fetchJson(){
+        val url = "https://superheroapi.com/api/2720451141562745/70"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body!!.string()
+                println(body)
+                val gson = GsonBuilder().create()
+                val homefeed = gson.fromJson(body,Hero::class.java)
+            }
+
+        })
     }
 
 
-
-    class AvengersAdapter:BaseAdapter{
+    class AvengersAdapter : BaseAdapter {
         var listOfAvengers = ArrayList<Avengers>()
-        var context:Context?=null
+        var context: Context? = null
 
-        constructor(context:Context,listOfAvengers:ArrayList<Avengers>):super(){
+        constructor(context: Context, listOfAvengers: ArrayList<Avengers>) : super() {
             this.listOfAvengers = listOfAvengers
             this.context = context
 
@@ -75,17 +86,18 @@ class MainActivity() : AppCompatActivity(), Parcelable {
         @SuppressLint("ViewHolder", "InflateParams")
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
             val avenger = listOfAvengers[position]
-            var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            var myView = inflater.inflate(R.layout.avengers_ticket,null)
+            var inflater =
+                context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var myView = inflater.inflate(R.layout.avengers_ticket, null)
             myView.tvName.text = avenger.name!!
             myView.tvDes.text = avenger.des!!
             myView.ivBar.setImageResource(avenger.image!!)
             myView.ticketAvenger.setOnClickListener {
-                val intent = Intent(context,moreAbout::class.java)
-                intent.putExtra("name",avenger.name!!)
-                intent.putExtra("des",avenger.des!!)
-                intent.putExtra("image",avenger.image!!)
-                intent.putExtra("subtitle",avenger.subtitle)
+                val intent = Intent(context, moreAbout::class.java)
+                intent.putExtra("name", avenger.name!!)
+                intent.putExtra("des", avenger.des!!)
+                intent.putExtra("image", avenger.image!!)
+                intent.putExtra("subtitle", avenger.subtitle)
                 context!!.startActivity(intent)
 
             }
@@ -94,75 +106,37 @@ class MainActivity() : AppCompatActivity(), Parcelable {
         }
     }
 
-    fun dummyData(){
+    fun dummyData() {
         listOfAvengers.add(
-            Avengers("Iron Man","Leader of Avengers"
-                ,R.drawable.ironman,R.string.ironman.toString()))
+            Avengers(
+                "Iron Man", "Leader of Avengers", R.drawable.ironman, R.string.ironman.toString()
+            )
+        )
         listOfAvengers.add(
-            Avengers("Thor","God of Thunder"
-                ,R.drawable.thor,R.string.thor.toString()))
+            Avengers(
+                "Thor", "God of Thunder", R.drawable.thor, R.string.thor.toString()
+            )
+        )
         listOfAvengers.add(
-            Avengers("Hulk","The gamma machine"
-                ,R.drawable.hulk,R.string.hulk.toString()))
+            Avengers(
+                "Hulk", "The gamma machine", R.drawable.hulk, R.string.hulk.toString()
+            )
+        )
         listOfAvengers.add(
-            Avengers("Hawkeye","Unfuckwithable archer"
-                ,R.drawable.hawkeye,R.string.hawkeye.toString()))
+            Avengers(
+                "Hawkeye", "Unfuckwithable archer", R.drawable.hawkeye, R.string.hawkeye.toString()
+            )
+        )
         listOfAvengers.add(
-            Avengers("Captain America","Super Soldier"
-                ,R.drawable.captain,R.string.captain.toString()))
+            Avengers(
+                "Captain America", "Super Soldier", R.drawable.captain, R.string.captain.toString()
+            )
+        )
         listOfAvengers.add(
-            Avengers("Black Widow","Shield Spy"
-                ,R.drawable.widow,R.string.widow.toString()))
+            Avengers(
+                "Black Widow", "Shield Spy", R.drawable.widow, R.string.widow.toString()
+            )
+        )
     }
-
-    fun run(url:String){
-        progress.visibility = View.VISIBLE
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                progress.visibility = View.GONE
-
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                var str_response = response.body!!.string()
-                //creating json object
-                val json_contact: JSONObject = JSONObject(str_response)
-                //creating json array
-                var jsonarray_info: JSONArray = json_contact.getJSONArray("info")
-                var i:Int = 0
-                var size:Int = jsonarray_info.length()
-                listOfAvengers= ArrayList<Avengers>();
-                for (i in 0.. size-1) {
-                    var json_objectdetail:JSONObject=jsonarray_info.getJSONObject(i)
-                    val id=json_objectdetail.getString("id")
-                    val name=json_objectdetail.getString("name")
-                    val email=json_objectdetail.getString("email")
-                    var avenger = Avengers(name,id,12,email)
-                    listOfAvengers.add(avenger)
-                }
-        }
-
-            )}
 }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MainActivity> {
-        override fun createFromParcel(parcel: Parcel): MainActivity {
-            return MainActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MainActivity?> {
-            return arrayOfNulls(size)
-        }
-    }
