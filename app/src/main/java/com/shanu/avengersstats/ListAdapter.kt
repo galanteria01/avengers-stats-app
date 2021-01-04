80b57c25234a6a63da38b8f1513d3ea0ee1ef580
galanteria01 <shanuu12e@gmail.com>
2021-01-04 09:53:00 +0530

    Live response added

--- app/src/main/java/com/shanu/avengersstats/ListAdapter.kt
+++ app/src/main/java/com/shanu/avengersstats/ListAdapter.kt
@@ -1,21 +1,33 @@
 package com.shanu.avengersstats
 
 import Hero
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.shanu.avengersstats.data.model.MainViewModel
 
-class ListAdapter(private val viewModel: MainViewModel,private val listOfHeroes:List<Hero>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class ListAdapter(private val viewModel: MainViewModel,private val listOfHeroes:List<Hero>):RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ViewHolder {
+        val myView = LayoutInflater.from(parent.context).inflate(R.layout.avengers_ticket,parent,false)
+        return ViewHolder(myView)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        return holder.bind(listOfHeroes[position])
     }
 
     override fun getItemCount(): Int {
         return listOfHeroes.size
+    }
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val heroName = itemView.findViewById<TextView>(R.id.tvName)
+        fun bind(hero:Hero){
+            heroName.text = hero.name.toString()
+
+        }
     }
 
 }
--- app/src/main/java/com/shanu/avengersstats/MainActivity.kt
+++ app/src/main/java/com/shanu/avengersstats/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ProgressBar
 import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.shanu.avengersstats.data.model.MainViewModel
+import com.shanu.avengersstats.data.model.MainViewModelFactory
+import com.shanu.avengersstats.data.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.avengers_ticket.*
 import kotlinx.android.synthetic.main.avengers_ticket.view.*
@@ -27,18 +33,22 @@ import java.net.URI.create
 import java.util.EnumSet.range
 
 class MainActivity() : AppCompatActivity() {
-
+    private lateinit var viewModel: MainViewModel
+    var adapter:ListAdapter?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getHero()
+        viewModel.myHeroData.observe(this, Observer { response ->
+            if(response.isSuccessful){
+                Log.d("avenger",response.body()!!.toString())
 
+
+            }
+        })
     }
-
-
-
-
-
-
-
 }
 
